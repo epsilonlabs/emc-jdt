232ce28ec8ff9e08059accea7ec3a2061509b1ea
Antonio García-Domínguez <nyoescape@gmail.com>
2016-07-17 23:12:53 +0100

    emc-jdt: add example, reparse with ASTParser to turn ast TD to dom TD

--- org.eclipse.epsilon.emc.jdt/src/org/eclipse/epsilon/emc/jdt/SearchableTypeCollection.java
+++ org.eclipse.epsilon.emc.jdt/src/org/eclipse/epsilon/emc/jdt/SearchableTypeCollection.java
@@ -6,7 +6,6 @@ import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.epsilon.eol.dom.EqualsOperatorExpression;
 import org.eclipse.epsilon.eol.dom.Expression;
@@ -16,9 +15,13 @@ import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
 import org.eclipse.epsilon.eol.execute.context.IEolContext;
 import org.eclipse.epsilon.eol.execute.operations.AbstractOperation;
 import org.eclipse.epsilon.eol.execute.operations.declarative.IAbstractOperationContributor;
+import org.eclipse.epsilon.eol.execute.operations.declarative.SelectOperation;
 import org.eclipse.jdt.core.IJavaProject;
-import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.search.IJavaSearchConstants;
 import org.eclipse.jdt.core.search.IJavaSearchScope;
 import org.eclipse.jdt.core.search.SearchEngine;
@@ -26,17 +29,7 @@ import org.eclipse.jdt.core.search.SearchMatch;
 import org.eclipse.jdt.core.search.SearchParticipant;
 import org.eclipse.jdt.core.search.SearchPattern;
 import org.eclipse.jdt.core.search.SearchRequestor;
-import org.eclipse.jdt.internal.compiler.CompilationResult;
-import org.eclipse.jdt.internal.compiler.DefaultErrorHandlingPolicies;
-import org.eclipse.jdt.internal.compiler.ast.CompilationUnitDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.TypeDeclaration;
-import org.eclipse.jdt.internal.compiler.env.ISourceType;
-import org.eclipse.jdt.internal.compiler.impl.CompilerOptions;
-import org.eclipse.jdt.internal.compiler.parser.SourceTypeConverter;
-import org.eclipse.jdt.internal.compiler.problem.DefaultProblemFactory;
-import org.eclipse.jdt.internal.compiler.problem.ProblemReporter;
-import org.eclipse.jdt.internal.core.BinaryType;
-import org.eclipse.jdt.internal.core.BinaryTypeConverter;
 import org.eclipse.jdt.internal.core.SourceType;
 
 /**
@@ -46,81 +39,70 @@ import org.eclipse.jdt.internal.core.SourceType;
 @SuppressWarnings("restriction")
 public class SearchableTypeCollection extends AbstractCollection<Object> implements IAbstractOperationContributor {
 
-	private static final long serialVersionUID = 1L;
-
-	private final class SelectSearchParticipant extends AbstractOperation {
+	private final class SelectSearchParticipant extends SelectOperation {
 		@Override
-		public Object execute(Object target,
-				NameExpression operationNameExpression, List<Parameter> iterators,
-				List<Expression> expressions, IEolContext context)
-				throws EolRuntimeException {
+		public Object execute(Object target, NameExpression operationNameExpression, List<Parameter> iterators,
+				List<Expression> expressions, IEolContext context) throws EolRuntimeException {
 
 			EqualsOperatorExpression equalsOperatorExpression = (EqualsOperatorExpression) expressions.get(0);
 
-			SearchPattern pattern = SearchPattern.createPattern(
-					context.getExecutorFactory().executeAST(equalsOperatorExpression.getSecondOperand(), context) + "", IJavaSearchConstants.TYPE,
-					IJavaSearchConstants.DECLARATIONS,
-					SearchPattern.R_PATTERN_MATCH
-							| SearchPattern.R_CASE_SENSITIVE);
-			
-			IJavaSearchScope scope = SearchEngine.createJavaSearchScope( javaProjects, IJavaSearchScope.SOURCES);
-			
-			SearchEngine engine = new SearchEngine(); 
+			SearchPattern pattern = SearchPattern
+					.createPattern(
+							context.getExecutorFactory().executeAST(equalsOperatorExpression.getSecondOperand(),
+									context) + "",
+							IJavaSearchConstants.TYPE, IJavaSearchConstants.DECLARATIONS,
+							SearchPattern.R_PATTERN_MATCH | SearchPattern.R_CASE_SENSITIVE);
+
+			IJavaSearchScope scope = SearchEngine.createJavaSearchScope(javaProjects, IJavaSearchScope.SOURCES);
+
+			SearchEngine engine = new SearchEngine();
 			try {
 				final List<Object> results = new ArrayList<Object>();
-				engine.search(pattern, new SearchParticipant[] { SearchEngine
-						.getDefaultSearchParticipant() }, scope, new SearchRequestor() {
+				engine.search(pattern, new SearchParticipant[] { SearchEngine.getDefaultSearchParticipant() }, scope,
+						new SearchRequestor() {
 
 							@Override
 							public void acceptSearchMatch(SearchMatch match) throws CoreException {
 								/*
-								 * Note: this code is based on the org.eclipse.jdt.internal.codeassist.CompletionEngine class,
-								 * particularly the complete(...) method.
+								 * Note: DOM nodes and internal nodes seem to be
+								 * created through different processes, but we
+								 * have the ICompilationUnit in common.
 								 */
-								if (match.getElement() instanceof IType) {
-									final IType type = (IType) match.getElement();
-									final IResource resource = type.getCompilationUnit().getResource();
+								if (match.getElement() instanceof SourceType) {
+									final SourceType srcType = (SourceType) match.getElement();
 
-									final ProblemReporter problemReporter = new ProblemReporter(DefaultErrorHandlingPolicies.ignoreAllProblems(), new CompilerOptions(), new DefaultProblemFactory());
-									final CompilationResult compilationResult = new CompilationResult(resource.getName().toCharArray(), 1, 1, 0);
+									/*
+									 * TODO: so far, it seems we need to parse
+									 * the file.
+									 *
+									 * TODO: take into account nested types /
+									 * package names (how to get fully qualified
+									 * name from DOM node?)
+									 */
 
-									if (type instanceof SourceType) {
-										convertSourceType(results, (SourceType) match.getElement(), problemReporter, compilationResult);
-									} else if (type instanceof BinaryType) {
-										convertBinaryType(results, type, problemReporter, compilationResult);
-									}
-								}
-								else {
+									ASTParser parser = ASTParser.newParser(AST.JLS8);
+									parser.setKind(ASTParser.K_COMPILATION_UNIT);
+									parser.setResolveBindings(true);
+									parser.setSource(srcType.getCompilationUnit());
+
+									ASTNode domAST = parser.createAST(null);
+									domAST.accept(new ASTVisitor() {
+										@Override
+										public boolean visit(org.eclipse.jdt.core.dom.TypeDeclaration node) {
+											final String domName = node.getName().getIdentifier();
+											if (domName.equals(srcType.getElementName())) {
+												results.add(node);
+											}
+											return true;
+										}
+									});
+								} else {
 									results.add(match.getElement());
-								}
-							}
-
-							private void convertBinaryType(final List<Object> results, final IType type,
-									final ProblemReporter problemReporter,
-									final CompilationResult compilationResult) throws JavaModelException {
-								final CompilationUnitDeclaration compilationUnit = new CompilationUnitDeclaration(problemReporter, compilationResult, 0);
-								final TypeDeclaration typeDeclaration = new BinaryTypeConverter(problemReporter, compilationResult, null /* no need to remember type names */).buildTypeDeclaration(type, compilationUnit);
-								results.add(typeDeclaration);
-							}
-
-							private void convertSourceType(final List<Object> results, final SourceType sourceType, final ProblemReporter problemReporter, CompilationResult compilationResult) throws JavaModelException {
-								final ISourceType info = (ISourceType) sourceType.getElementInfo();
-								final CompilationUnitDeclaration compilationUnit = SourceTypeConverter.buildCompilationUnit(
-									new ISourceType[] {info},//sourceTypes[0] is always toplevel here
-									SourceTypeConverter.FIELD_AND_METHOD // need field and methods
-									| SourceTypeConverter.MEMBER_TYPE, // need member types
-									problemReporter, // no need for field initialization
-									compilationResult);
-
-								if (compilationUnit.types != null) {
-									TypeDeclaration typeDeclaration = compilationUnit.types[0];
-									results.add(typeDeclaration);
 								}
 							}
 						}, null);
 				return results;
-			}
-			catch (Exception ex) {
+			} catch (Exception ex) {
 				throw new RuntimeException(ex);
 			}
 		}
@@ -128,20 +110,23 @@ public class SearchableTypeCollection extends AbstractCollection<Object> impleme
 
 	protected IJavaProject[] javaProjects = null;
 
-	/** The visitor is only used if the script tries to iterate through the entire TypeDeclaration.all list. */
+	/**
+	 * The visitor is only used if the script tries to iterate through the
+	 * entire TypeDeclaration.all list.
+	 */
 	protected ReflectiveASTVisitor visitor;
-	
+
 	public SearchableTypeCollection(IJavaProject[] javaProjects, ReflectiveASTVisitor visitor) {
 		this.javaProjects = javaProjects;
 		this.visitor = visitor;
 	}
-	
+
 	@Override
 	public AbstractOperation getAbstractOperation(String name) {
 		if ("select".equals(name)) {
 			return new SelectSearchParticipant();
 		}
-		
+
 		return null;
 	}
 
